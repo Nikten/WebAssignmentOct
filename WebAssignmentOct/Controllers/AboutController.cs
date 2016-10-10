d6ea6cc0d26c0472a1f76dcb8f445a6380c55aed
daniel svanber <daniel.nikten@gmail.com>
2016-10-10 10:04:02 +0200

    Added new views for about me.

---
+++ WebAssignmentOct/Controllers/AboutController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAssignmentOct.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        public ActionResult educatuation()
+        {
+            return View();
+        }
+
+        public ActionResult Exprience()
+        {
+            return View();
+        }
+        
+        public ActionResult Knolege()
+        {
+            return View();
+        }
+    }
+}
