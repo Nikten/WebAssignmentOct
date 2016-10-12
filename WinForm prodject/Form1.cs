a665b0aeaedeb116427488680a4bc922755e3b02
daniel svanber <daniel.nikten@gmail.com>
2016-10-12 09:39:21 +0200

    Listbox now desplais the correct mane istead of the form.

---
+++ WinForm prodject/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm_prodject
+{
+    public partial class Form1 : Form
+    {
+        WebDataBaseEntities MyEduaction = new WebDataBaseEntities();
+        public Form1()
+        {
+            InitializeComponent();
+            foreach (var item in MyEduaction.Educations)
+            {
+                listBox1.Items.Add(item);
+                listBox1.DisplayMember = "HaveStudiedOn";
+            }
+        }
+
+    }
+}
